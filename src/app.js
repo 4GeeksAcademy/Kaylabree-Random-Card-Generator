82741cd97939b8c6e103b79e27cad1e26e91d2e3
Kaylabree <111409668+Kaylabree@users.noreply.github.com>
2024-06-19 01:06:42 +0000

    project done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,39 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+const generateRandomcard = () => {
+  let values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "j",
+    "q",
+    "k",
+    "a"
+  ];
+  let suits = ["♦", "♥", "♠", "♣"];
+  let randomvalue = values[Math.floor(Math.random() * values.length)];
+  let randomsuits = suits[Math.floor(Math.random() * suits.length)];
+  if (randomsuits === "♥" || randomsuits === "♦") {
+    document.querySelector(".top-suit").style.color = "red";
+    document.querySelector(".bottom-suit").style.color = "red";
+  } else {
+    document.querySelector(".top-suit").style.color = "black";
+    document.querySelector(".bottom-suit").style.color = "black";
+  }
+  document.querySelector(".top-suit").innerHTML = randomsuits;
+  document.querySelector(".number").innerHTML = randomvalue;
+  document.querySelector(".bottom-suit").innerHTML = randomsuits;
+};
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".btn").addEventListener("click", function() {
+    generateRandomcard();
+  });
 };
